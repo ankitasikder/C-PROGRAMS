2fb800c10c734770d536554fa65140fde09e7b15
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 21:38:47 +0530

    Update takingvaluepointer.c

--- Pointer/takingvaluepointer.c
+++ Pointer/takingvaluepointer.c
@@ -1,4 +1,14 @@
 //WAP to store a value using pointer
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
