a91dbbec07ab9bf01ff7ba18b9413869f68e7cef
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 21:37:48 +0530

    Update copyaraypointer.c

--- Pointer/copyaraypointer.c
+++ Pointer/copyaraypointer.c
@@ -1,4 +1,14 @@
 //WAp to copy an array's value into another array using pointer
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
