7db0535abb079ea72cf5d308427801b5bb2d0337
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 21:27:34 +0530

    ADDED BY ANKITA

---
+++ structure/array of struct.c
@@ -0,0 +1,31 @@
+//array of structure
+#include <stdio.h>
+#include <string.h>
+struct student{
+   int id;
+   char name[30];
+   float percentage;
+};
+int main(){
+   int i;
+   struct student record[2];
+   // 1st student's record
+   record[0].id=1;
+   strcpy(record[0].name, "Ankita");
+   record[0].percentage = 86;
+   // 2nd student's record
+   record[1].id=2;
+   strcpy(record[1].name, "Biswa");
+   record[1].percentage = 90;
+   // 3rd student's record
+   record[2].id=3;
+   strcpy(record[2].name, "Mona");
+   record[2].percentage = 95;
+   for(i=0; i<3; i++){
+      printf(" Records of STUDENT : %d \n", i+1);
+      printf(" Id is: %d \n", record[i].id);
+      printf(" Name is: %s \n", record[i].name);
+      printf(" Percentage is: %f\n\n",record[i].percentage);
+   }
+   return 0;
+}
