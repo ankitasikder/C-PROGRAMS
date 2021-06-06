5b407484b8eedab33287fe1ad5c7ded55bf32404
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 21:36:35 +0530

    Update access lelements with pointers.c

--- Pointer/access lelements with pointers.c
+++ Pointer/access lelements with pointers.c
@@ -1,4 +1,14 @@
 //access array elements using pointers
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<stdio.h>
 #include<stdio.h>
 void main()
