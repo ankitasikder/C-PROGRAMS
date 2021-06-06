f4ce88feb5c900c44bca8382504e2c75793b5ac9
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 18:51:06 +0530

    Update swaping.c

--- Checking and Loops Problems/swaping.c
+++ Checking and Loops Problems/swaping.c
@@ -1,4 +1,14 @@
 //swap without 3rd variable
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
