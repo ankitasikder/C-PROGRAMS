53535f9b1c8f5b47fc7ff392f0633036f3010046
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 18:36:24 +0530

    Update sizeof.c

--- Assignment-1/sizeof.c
+++ Assignment-1/sizeof.c
@@ -1,4 +1,14 @@
 //Write a C program to print size of variables using sizeof() operator
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 int main()
 {
