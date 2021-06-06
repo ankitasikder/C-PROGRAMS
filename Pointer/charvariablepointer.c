d9992c444886b22015ff6c0a30367f49427bd9d6
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 21:37:13 +0530

    Update charvariablepointer.c

--- Pointer/charvariablepointer.c
+++ Pointer/charvariablepointer.c
@@ -1,4 +1,14 @@
 //WAP to store character & print the char using pouinter variable
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
