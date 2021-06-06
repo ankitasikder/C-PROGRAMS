aeffcd9ad16dee12c8b04441de755f4c0a3aaf8f
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 18:35:15 +0530

    Update as1pr5.c

--- Assignment-1/as1pr5.c
+++ Assignment-1/as1pr5.c
@@ -1,4 +1,14 @@
 //Write a C program to find area of a rectangle
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
