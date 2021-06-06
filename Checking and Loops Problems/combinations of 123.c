8b65d8d0a1deb395e27e556d7814a8ec4ab03460
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 18:48:36 +0530

    Update combinations of 123.c

--- Checking and Loops Problems/combinations of 123.c
+++ Checking and Loops Problems/combinations of 123.c
@@ -1,4 +1,14 @@
 //WAP to print all the possible combinations of 1,2,3
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
