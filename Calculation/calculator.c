bd950301230e9ceb5f46d42662d4f9176d8bb5c8
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 18:38:55 +0530

    Update calculator.c

--- Calculation/calculator.c
+++ Calculation/calculator.c
@@ -1,4 +1,14 @@
 //create a calculator
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
