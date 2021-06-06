91bb29a52a3f286b7e6afb271195c8bf46af3dba
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 18:34:46 +0530

    Update as1pr1.c

--- Assignment-1/as1pr1.c
+++ Assignment-1/as1pr1.c
@@ -1,4 +1,16 @@
 //1.Write a simple program in C to make use of all the arithmatic operators(+,-,*,%)
+
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
+
 #include<Stdio.h>
 #include<conio.h>
 void main()
