7ec493608f4e22453b16ccaa0571309aed3fb78b
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 18:35:52 +0530

    Update inchfeet.c

--- Assignment-1/inchfeet.c
+++ Assignment-1/inchfeet.c
@@ -1,4 +1,14 @@
 //Write a C program to convert feet to inches and vice versa
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
