e6fda962840dc78df045474e014278a83f8ecbe9
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 18:36:07 +0530

    Update multiply using plus.c

--- Assignment-1/multiply using plus.c
+++ Assignment-1/multiply using plus.c
@@ -1,4 +1,14 @@
 //Write a C program to multiply two numbers using plus operator
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
