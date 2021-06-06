34c4e804f41190ff054b1c901ed9afe3a68c2e88
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-06 19:29:07 +0530

    Update sorting array elements.c

--- ARRAYS/sorting array elements.c
+++ ARRAYS/sorting array elements.c
@@ -1,4 +1,14 @@
 //SORTING ARRAY ELEMENTS
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<stdio.h>
 #define MAXSIZE 10
 void main()
