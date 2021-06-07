dcbbca3afcda405fb607ff95de3c7745a3a23da5
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-07 20:57:22 +0530

    Update name1stletter2.c

--- Strings/name1stletter2.c
+++ Strings/name1stletter2.c
@@ -1,4 +1,14 @@
 //acromating
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 #include<string.h>
