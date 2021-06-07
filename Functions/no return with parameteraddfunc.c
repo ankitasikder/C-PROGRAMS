d0d08764b3bc552fddd75834e9bf5e7e85342154
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-07 20:32:02 +0530

    Update no return with parameteraddfunc.c

--- Functions/no return with parameteraddfunc.c
+++ Functions/no return with parameteraddfunc.c
@@ -1,4 +1,14 @@
 //no return type & with parameter
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void calc(int,int);//declaration
