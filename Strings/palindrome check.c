6e17b7c3bc2d043e6b7a62688d6fa38237a591af
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-07 20:58:10 +0530

    Update palindrome check.c

--- Strings/palindrome check.c
+++ Strings/palindrome check.c
@@ -1,4 +1,14 @@
 //check if a string is palindrome
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
