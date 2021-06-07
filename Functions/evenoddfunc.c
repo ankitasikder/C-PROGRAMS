f533891a57c28aaa1a67a379f9a7dfe6fff6b1f7
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-07 20:28:36 +0530

    Update evenoddfunc.c

--- Functions/evenoddfunc.c
+++ Functions/evenoddfunc.c
@@ -1,4 +1,14 @@
 //WAP to check is even or odd no return type no parameter
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void evenodd();//declaration 
