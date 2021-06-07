428627752c356157e2e5ce247da2feffd17e87ce
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-07 20:34:26 +0530

    Update squarefunc.c

--- Functions/squarefunc.c
+++ Functions/squarefunc.c
@@ -1,4 +1,14 @@
 //WAP to print square of a no. using no return type no parameter 
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void sq();//declaration
