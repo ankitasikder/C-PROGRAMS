5cb7045676c0309cec8ea976c81bda717fd5c462
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-07 20:29:09 +0530

    Update factorialfunc.c

--- Functions/factorialfunc.c
+++ Functions/factorialfunc.c
@@ -1,4 +1,14 @@
 //WAP to print factorial of no, return type with parameter passing 
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void fact(int);//declaratyion 
