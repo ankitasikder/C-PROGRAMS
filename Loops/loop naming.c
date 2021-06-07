025bb36c9c04636c1058789a25bba191b1f24c23
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-07 21:08:38 +0530

    Update loop naming.c

--- Loops/loop naming.c
+++ Loops/loop naming.c
@@ -1,4 +1,14 @@
 //WAP to print your name 10 times using decrement
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 #include<Stdio.h>
 #include<conio.h>
 void main()
